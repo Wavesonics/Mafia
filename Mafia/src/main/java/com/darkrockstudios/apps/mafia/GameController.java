966dd76a854e0d5a0a8c4211273871d80d7b35c5
Wavesonics <adamwbrown@gmail.com>
2014-04-27 02:22:12 -0700

    - Work to make the game session survive configuration changes

--- Mafia/src/main/java/com/darkrockstudios/apps/mafia/GameController.java
+++ Mafia/src/main/java/com/darkrockstudios/apps/mafia/GameController.java
@@ -1,17 +1,41 @@
 package com.darkrockstudios.apps.mafia;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.GamesActivityResultCodes;
+import com.google.android.gms.games.GamesStatusCodes;
+import com.google.android.gms.games.multiplayer.Invitation;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.OnInvitationReceivedListener;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessage;
 import com.google.android.gms.games.multiplayer.realtime.RealTimeMessageReceivedListener;
 import com.google.android.gms.games.multiplayer.realtime.Room;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
+import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
+import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
 import com.google.example.games.basegameutils.GameHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Adam on 4/26/2014.
  */
-public class GameController implements RealTimeMessageReceivedListener
+public class GameController extends Fragment implements OnInvitationReceivedListener, RoomUpdateListener, RoomStatusUpdateListener, RealTimeMessageReceivedListener, GameHelper.GameHelperListener
 {
-	private static final String FRAGTAG = GameController.class.getName() + ".GAMECONTROLLER";
+	private static final String FRAGTAG             = GameController.class.getName() + ".GAMECONTROLLER";
+	private final static String FRAGTAG_INVITATIONS = MainActivity.class.getPackage() + ".INVITATIONS";
+	private final static String FRAGTAG_GAME        = MainActivity.class.getPackage() + ".GAME";
+
+	protected MainActivity m_activity = null;
 
 	// Request code for the "select players" UI
 	public final static int RC_SELECT_PLAYERS   = 10000;
@@ -21,17 +45,10 @@ public class GameController implements RealTimeMessageReceivedListener
 	public final static int MIN_PLAYERS = 2;
 	public final static int MAX_PLAYERS = 8;
 
-	private final GameHelper      m_gameHelper;
-	private       GoogleApiClient m_apiClient;
+	private GameHelper m_gameHelper;
 
 	private Room m_room;
 
-	public GameController( final GameHelper gameHelper, final GoogleApiClient apiClient )
-	{
-		m_gameHelper = gameHelper;
-		m_apiClient = apiClient;
-	}
-	/*
 	public static GameController get( final FragmentManager fragmentManager )
 	{
 		final GameController gameController;
@@ -51,26 +68,132 @@ public class GameController implements RealTimeMessageReceivedListener
 	}
 
 	@Override
-	public void onAttach( Activity activity )
+	public void onAttach( final Activity activity )
 	{
 		super.onAttach( activity );
 
 		if( activity instanceof MainActivity )
 		{
-			MainActivity mainActivity = (MainActivity) activity;
-
-			m_gameHelper = mainActivity.getGameHelper();
-			m_apiClient = mainActivity.getGoogleApiClient();
+			m_activity = (MainActivity) activity;
 		}
 	}
 
 	@Override
-	public void onCreate( Bundle savedInstanceState )
+	public void onDestroy()
+	{
+		Log.d( "mab", this + ": onDestroy()" );
+		super.onDestroy();
+		m_gameHelper.onStop();
+	}
+
+	@Override
+	public void onDetach()
+	{
+		super.onDetach();
+		m_activity = null;
+	}
+
+	@Override
+	public void onCreate( final Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
 		setRetainInstance( true );
+
+		m_gameHelper = new GameHelper( m_activity, GameHelper.CLIENT_GAMES );
+		m_gameHelper.setup( this );  //'this' => GameHelperListener
+
+		//m_gameHelper.setSigningInMessage(mSigningInMessage);
+		//m_gameHelper.setSigningOutMessage(mSigningOutMessage);
+		m_gameHelper.onStart( m_activity );
+
+		gotoInvitationsScreen();
 	}
-	*/
+
+	@Override
+	public void onActivityResult( final int requestCode, final int responseCode, final Intent data )
+	{
+		super.onActivityResult( requestCode, responseCode, data );
+		m_gameHelper.onActivityResult( requestCode, responseCode, data );
+
+		if( requestCode == GameController.RC_SELECT_PLAYERS )
+		{
+			if( responseCode != Activity.RESULT_OK )
+			{
+				// user canceled
+				return;
+			}
+
+			// Get the invitee list
+			Bundle extras = data.getExtras();
+			final ArrayList<String> invitees = data.getStringArrayListExtra( Games.EXTRA_PLAYER_IDS );
+
+			// create the room and specify a variant if appropriate
+			RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
+			roomConfigBuilder.addPlayersToInvite( invitees );
+
+			RoomConfig roomConfig = roomConfigBuilder.build();
+			Games.RealTimeMultiplayer.create( getApiClient(), roomConfig );
+
+			// prevent screen from sleeping during handshake
+			m_activity.getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+		}
+		else if( requestCode == GameController.RC_WAITING_ROOM )
+		{
+			if( responseCode == Activity.RESULT_OK )
+			{
+				// (start game)
+				gotoGameScreen();
+			}
+			else if( responseCode == Activity.RESULT_CANCELED )
+			{
+				// Waiting room was dismissed with the back button. The meaning of this
+				// action is up to the game. You may choose to leave the room and cancel the
+				// match, or do something else like minimize the waiting room and
+				// continue to connect in the background.
+
+				// in this example, we take the simple approach and just leave the room:
+				Games.RealTimeMultiplayer.leave( getApiClient(), this, getRoom().getRoomId() );
+				m_activity.getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+			}
+			else if( responseCode == GamesActivityResultCodes.RESULT_LEFT_ROOM )
+			{
+				// player wants to leave the room.
+				Games.RealTimeMultiplayer.leave( getApiClient(), this, getRoom().getRoomId() );
+				m_activity.getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+			}
+		}
+		if( requestCode == GameController.RC_INVITATION_INBOX )
+		{
+			if( responseCode != Activity.RESULT_OK )
+			{
+				// canceled
+				return;
+			}
+
+			// get the selected invitation
+			Bundle extras = data.getExtras();
+			Invitation invitation = extras.getParcelable( Multiplayer.EXTRA_INVITATION );
+
+			// accept it!
+			RoomConfig roomConfig = makeBasicRoomConfigBuilder()
+					                        .setInvitationIdToAccept( invitation.getInvitationId() )
+					                        .build();
+			Games.RealTimeMultiplayer.join( getApiClient(), roomConfig );
+
+			// prevent screen from sleeping during handshake
+			m_activity.getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+
+			// go to game screen
+		}
+	}
+
+	// create a RoomConfigBuilder that's appropriate for your implementation
+	private RoomConfig.Builder makeBasicRoomConfigBuilder()
+	{
+		return RoomConfig.builder( this )
+		                 .setMessageReceivedListener( this )
+		                 .setRoomStatusUpdateListener( this );
+	}
 
 	public void setRoom( final Room room )
 	{
@@ -89,11 +212,204 @@ public class GameController implements RealTimeMessageReceivedListener
 
 	public GoogleApiClient getApiClient()
 	{
-		return m_apiClient;
+		return m_gameHelper.getApiClient();
+	}
+
+	public void gotoInvitationsScreen()
+	{
+		InvitationsFragment fragment = InvitationsFragment.newInstance();
+		m_activity.getFragmentManager().beginTransaction().replace( R.id.container, fragment, FRAGTAG_INVITATIONS ).commit();
+	}
+
+	public void gotoInvitationInbox()
+	{
+		// launch the intent to show the invitation inbox screen
+		Intent intent = Games.Invitations.getInvitationInboxIntent( getApiClient() );
+		startActivityForResult( intent, GameController.RC_INVITATION_INBOX );
+	}
+
+	public void gotoWaitingRoom( final Room room )
+	{
+		Intent i = Games.RealTimeMultiplayer.getWaitingRoomIntent( getApiClient(), room, GameController.MIN_PLAYERS );
+		startActivityForResult( i, GameController.RC_WAITING_ROOM );
+	}
+
+	public void gotoSelectPlayers()
+	{
+		Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent( getApiClient(),
+		                                                                    GameController.MIN_PLAYERS - 1,
+		                                                                    GameController.MAX_PLAYERS - 1 );
+		startActivityForResult( intent, GameController.RC_SELECT_PLAYERS );
+	}
+
+	public void gotoGameScreen()
+	{
+		GameFragment fragment = GameFragment.newInstance();
+		m_activity.getFragmentManager().beginTransaction().replace( R.id.container, fragment, FRAGTAG_GAME ).commit();
 	}
 
 	@Override
-	public void onRealTimeMessageReceived( RealTimeMessage realTimeMessage )
+	public void onRealTimeMessageReceived( final RealTimeMessage realTimeMessage )
+	{
+
+	}
+
+	@Override
+	public void onRoomCreated( final int statusCode, final Room room )
+	{
+		if( statusCode != GamesStatusCodes.STATUS_OK )
+		{
+			// let screen go to sleep
+			m_activity.getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+
+			// show error message, return to main screen.
+			m_activity.displayError( "Game Created" );
+		}
+		else
+		{
+			setRoom( room );
+			m_activity.displayConfirm( "Game Created" );
+			gotoWaitingRoom( room );
+		}
+	}
+
+	@Override
+	public void onJoinedRoom( final int statusCode, final Room room )
+	{
+		if( statusCode != GamesStatusCodes.STATUS_OK )
+		{
+			// let screen go to sleep
+			m_activity.getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+
+			// show error message, return to main screen.
+			m_activity.displayError( "Failed to join Game" );
+		}
+		else
+		{
+			setRoom( room );
+			m_activity.displayConfirm( "Game Joined" );
+			gotoWaitingRoom( room );
+		}
+	}
+
+	@Override
+	public void onLeftRoom( final int i, final String s )
+	{
+		setRoom( null );
+	}
+
+	@Override
+	public void onRoomConnected( final int statusCode, final Room room )
+	{
+		if( statusCode != GamesStatusCodes.STATUS_OK )
+		{
+			// let screen go to sleep
+			m_activity.getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+
+			// show error message, return to main screen.
+			m_activity.displayError( "Failed to connect to game" );
+		}
+		else
+		{
+			m_activity.displayConfirm( "Connected to Game" );
+		}
+	}
+
+	@Override
+	public void onRoomConnecting( final Room room )
+	{
+		m_activity.displayInfo( "Connecting..." );
+	}
+
+	@Override
+	public void onRoomAutoMatching( final Room room )
+	{
+
+	}
+
+	@Override
+	public void onPeerInvitedToRoom( final Room room, final List<String> peers )
+	{
+		m_activity.displayInfo( "Invited player." );
+	}
+
+	@Override
+	public void onPeerDeclined( final Room room, final List<String> peers )
+	{
+		m_activity.displayInfo( "Invited player declined." );
+	}
+
+	@Override
+	public void onPeerJoined( final Room room, final List<String> peers )
+	{
+		m_activity.displayInfo( "Player Joined" );
+	}
+
+	@Override
+	public void onPeerLeft( final Room room, final List<String> peers )
+	{
+		m_activity.displayInfo( "Player Left" );
+	}
+
+	@Override
+	public void onConnectedToRoom( final Room room )
+	{
+
+	}
+
+	@Override
+	public void onDisconnectedFromRoom( final Room room )
+	{
+
+	}
+
+	@Override
+	public void onPeersConnected( final Room room, final List<String> strings )
+	{
+
+	}
+
+	@Override
+	public void onPeersDisconnected( final Room room, final List<String> strings )
+	{
+
+	}
+
+	@Override
+	public void onP2PConnected( final String s )
+	{
+
+	}
+
+	@Override
+	public void onP2PDisconnected( final String s )
+	{
+
+	}
+
+	@Override
+	public void onInvitationReceived( final Invitation invitation )
+	{
+
+	}
+
+	@Override
+	public void onInvitationRemoved( final String s )
+	{
+
+	}
+
+	@Override
+	public void onSignInFailed()
+	{
+		// If we fail sign in here, go back to the IntroActivity
+		Intent intent = new Intent( m_activity, IntroActivity.class );
+		startActivity( intent );
+		m_activity.finish();
+	}
+
+	@Override
+	public void onSignInSucceeded()
 	{
 
 	}
--- Mafia/src/main/java/com/darkrockstudios/apps/mafia/MainActivity.java
+++ Mafia/src/main/java/com/darkrockstudios/apps/mafia/MainActivity.java
@@ -1,81 +1,26 @@
 package com.darkrockstudios.apps.mafia;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.WindowManager;
-
-import com.google.android.gms.games.Games;
-import com.google.android.gms.games.GamesActivityResultCodes;
-import com.google.android.gms.games.GamesStatusCodes;
-import com.google.android.gms.games.multiplayer.Invitation;
-import com.google.android.gms.games.multiplayer.Multiplayer;
-import com.google.android.gms.games.multiplayer.realtime.Room;
-import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
-import com.google.android.gms.games.multiplayer.realtime.RoomStatusUpdateListener;
-import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
-import com.google.example.games.basegameutils.BaseGameActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.ButterKnife;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
-public class MainActivity extends BaseGameActivity implements RoomUpdateListener, RoomStatusUpdateListener, GameSetupHandler, GameControllerProvider
+public class MainActivity extends Activity implements GameSetupHandler, GameControllerProvider
 {
-	private final static String FRAGTAG_INVITATIONS = MainActivity.class.getPackage() + ".INVITATIONS";
-	private final static String FRAGTAG_GAME        = MainActivity.class.getPackage() + ".GAME";
-
 	private GameController m_gameController;
 
 	@Override
 	protected void onCreate( final Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
-
-		m_gameController = new GameController( getGameHelper(), getApiClient() );
-
 		setContentView( R.layout.activity_main );
 		ButterKnife.inject( this );
 
-		gotoInvitationsScreen();
-	}
-
-	private void gotoInvitationsScreen()
-	{
-		InvitationsFragment fragment = InvitationsFragment.newInstance();
-		getFragmentManager().beginTransaction().replace( R.id.container, fragment, FRAGTAG_INVITATIONS ).commit();
-	}
-
-	private void gotoInvitationInbox()
-	{
-		// launch the intent to show the invitation inbox screen
-		Intent intent = Games.Invitations.getInvitationInboxIntent( getApiClient() );
-		startActivityForResult( intent, GameController.RC_INVITATION_INBOX );
-	}
-
-	private void gotoWaitingRoom( final Room room )
-	{
-		Intent i = Games.RealTimeMultiplayer.getWaitingRoomIntent( getApiClient(), room, GameController.MIN_PLAYERS );
-		startActivityForResult( i, GameController.RC_WAITING_ROOM );
-	}
-
-	public void gotoSelectPlayers()
-	{
-		Intent intent = Games.RealTimeMultiplayer.getSelectOpponentsIntent( m_gameController.getApiClient(),
-		                                                                    GameController.MIN_PLAYERS - 1,
-		                                                                    GameController.MAX_PLAYERS - 1 );
-		startActivityForResult( intent, GameController.RC_SELECT_PLAYERS );
-	}
-
-	private void gotoGameScreen()
-	{
-		GameFragment fragment = GameFragment.newInstance();
-		getFragmentManager().beginTransaction().replace( R.id.container, fragment, FRAGTAG_GAME ).commit();
+		m_gameController = GameController.get( getFragmentManager() );
 	}
 
 	@Override
@@ -96,261 +41,32 @@ public class MainActivity extends BaseGameActivity implements RoomUpdateListener
 		return super.onOptionsItemSelected( item );
 	}
 
-	@Override
-	public void onSignInFailed()
-	{
-		// If we fail sign in here, go back to the IntroActivity
-		Intent intent = new Intent( this, IntroActivity.class );
-		startActivity( intent );
-		finish();
-	}
-
-	@Override
-	public void onSignInSucceeded()
-	{
-
-	}
-
-	@Override
-	public void onActivityResult( final int request, final int response, final Intent data )
-	{
-		super.onActivityResult( request, response, data );
-
-		if( request == GameController.RC_SELECT_PLAYERS )
-		{
-			if( response != Activity.RESULT_OK )
-			{
-				// user canceled
-				return;
-			}
-
-			// Get the invitee list
-			Bundle extras = data.getExtras();
-			final ArrayList<String> invitees = data.getStringArrayListExtra( Games.EXTRA_PLAYER_IDS );
-
-			// create the room and specify a variant if appropriate
-			RoomConfig.Builder roomConfigBuilder = makeBasicRoomConfigBuilder();
-			roomConfigBuilder.addPlayersToInvite( invitees );
-
-			RoomConfig roomConfig = roomConfigBuilder.build();
-			Games.RealTimeMultiplayer.create( getApiClient(), roomConfig );
-
-			// prevent screen from sleeping during handshake
-			getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
-		}
-		else if( request == GameController.RC_WAITING_ROOM )
-		{
-			if( response == Activity.RESULT_OK )
-			{
-				// (start game)
-				gotoGameScreen();
-			}
-			else if( response == Activity.RESULT_CANCELED )
-			{
-				// Waiting room was dismissed with the back button. The meaning of this
-				// action is up to the game. You may choose to leave the room and cancel the
-				// match, or do something else like minimize the waiting room and
-				// continue to connect in the background.
-
-				// in this example, we take the simple approach and just leave the room:
-				Games.RealTimeMultiplayer.leave( getApiClient(), this, m_gameController.getRoom().getRoomId() );
-				getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
-			}
-			else if( response == GamesActivityResultCodes.RESULT_LEFT_ROOM )
-			{
-				// player wants to leave the room.
-				Games.RealTimeMultiplayer.leave( getApiClient(), this, m_gameController.getRoom().getRoomId() );
-				getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
-			}
-		}
-		if( request == GameController.RC_INVITATION_INBOX )
-		{
-			if( response != Activity.RESULT_OK )
-			{
-				// canceled
-				return;
-			}
-
-			// get the selected invitation
-			Bundle extras = data.getExtras();
-			Invitation invitation = extras.getParcelable( Multiplayer.EXTRA_INVITATION );
-
-			// accept it!
-			RoomConfig roomConfig = makeBasicRoomConfigBuilder()
-					                        .setInvitationIdToAccept( invitation.getInvitationId() )
-					                        .build();
-			Games.RealTimeMultiplayer.join( getApiClient(), roomConfig );
-
-			// prevent screen from sleeping during handshake
-			getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
-
-			// go to game screen
-		}
-	}
-
-	// create a RoomConfigBuilder that's appropriate for your implementation
-	private RoomConfig.Builder makeBasicRoomConfigBuilder()
-	{
-		return RoomConfig.builder( this )
-		                 .setMessageReceivedListener( m_gameController )
-		                 .setRoomStatusUpdateListener( this );
-	}
-
-	@Override
-	public void onRoomCreated( final int statusCode, final Room room )
-	{
-		if( statusCode != GamesStatusCodes.STATUS_OK )
-		{
-			// let screen go to sleep
-			getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
-
-			// show error message, return to main screen.
-			displayError( "Game Created" );
-		}
-		else
-		{
-			m_gameController.setRoom( room );
-			displayConfirm( "Game Created" );
-			gotoWaitingRoom( room );
-		}
-	}
-
-	@Override
-	public void onJoinedRoom( final int statusCode, final Room room )
-	{
-		if( statusCode != GamesStatusCodes.STATUS_OK )
-		{
-			// let screen go to sleep
-			getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
-
-			// show error message, return to main screen.
-			displayError( "Failed to join Game" );
-		}
-		else
-		{
-			m_gameController.setRoom( room );
-			displayConfirm( "Game Joined" );
-			gotoWaitingRoom( room );
-		}
-	}
-
-	@Override
-	public void onLeftRoom( final int i, final String s )
-	{
-		m_gameController.setRoom( null );
-	}
-
-	@Override
-	public void onRoomConnected( final int statusCode, final Room room )
-	{
-		if( statusCode != GamesStatusCodes.STATUS_OK )
-		{
-			// let screen go to sleep
-			getWindow().clearFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
-
-			// show error message, return to main screen.
-			Crouton.makeText( this, "Connected to Game", Style.CONFIRM ).show();
-		}
-	}
-
-	@Override
-	public void onRoomConnecting( final Room room )
-	{
-		displayInfo( "Connecting..." );
-	}
-
-	@Override
-	public void onRoomAutoMatching( final Room room )
-	{
-
-	}
-
-	@Override
-	public void onPeerInvitedToRoom( final Room room, final List<String> peers )
-	{
-		displayInfo( "Invited player." );
-	}
-
-	@Override
-	public void onPeerDeclined( final Room room, final List<String> peers )
-	{
-		displayInfo( "Invited player declined." );
-	}
-
-	@Override
-	public void onPeerJoined( final Room room, final List<String> peers )
-	{
-		displayInfo( "Player Joined" );
-	}
-
-	@Override
-	public void onPeerLeft( final Room room, final List<String> peers )
-	{
-		displayInfo( "Player Left" );
-	}
-
-	@Override
-	public void onConnectedToRoom( final Room room )
-	{
-
-	}
-
-	@Override
-	public void onDisconnectedFromRoom( final Room room )
-	{
-
-	}
-
-	@Override
-	public void onPeersConnected( final Room room, final List<String> strings )
-	{
-
-	}
-
-	@Override
-	public void onPeersDisconnected( final Room room, final List<String> strings )
-	{
-
-	}
-
-	@Override
-	public void onP2PConnected( final String s )
-	{
-
-	}
-
-	@Override
-	public void onP2PDisconnected( final String s )
-	{
-
-	}
-
-	private void displayError( final int messageResource )
+	public void displayError( final int messageResource )
 	{
 		displayError( getString( messageResource ) );
 	}
 
-	private void displayError( final String message )
+	public void displayError( final String message )
 	{
 		displayMessage( message, Style.ALERT );
 	}
 
-	private void displayInfo( final int messageResource )
+	public void displayInfo( final int messageResource )
 	{
 		displayInfo( getString( messageResource ) );
 	}
 
-	private void displayInfo( final String message )
+	public void displayInfo( final String message )
 	{
 		displayMessage( message, Style.INFO );
 	}
 
-	private void displayConfirm( final int messageResource )
+	public void displayConfirm( final int messageResource )
 	{
 		displayConfirm( getString( messageResource ) );
 	}
 
-	private void displayConfirm( final String message )
+	public void displayConfirm( final String message )
 	{
 		displayMessage( message, Style.CONFIRM );
 	}
@@ -363,13 +79,13 @@ public class MainActivity extends BaseGameActivity implements RoomUpdateListener
 	@Override
 	public void createGame()
 	{
-		gotoSelectPlayers();
+		m_gameController.gotoSelectPlayers();
 	}
 
 	@Override
 	public void joinGame()
 	{
-		gotoInvitationInbox();
+		m_gameController.gotoInvitationInbox();
 	}
 
 	@Override
